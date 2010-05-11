6fb9da7646f06b38ccb3f27f75a820666092cf40
whalebot.helmsman <whalebot.helmsman@bb6d7b18-8b01-a3bd-bbd2-4e41f94b8a27>
2010-05-11 19:05:13 +0000

    commiting current version of google-url

--- trunk/url-parsing-benchmark/google-url/gurl.sh
+++ trunk/url-parsing-benchmark/google-url/gurl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-svn co http://google-url.googlecode.com/svn/trunk/ googleurl
+svn export http://google-url.googlecode.com/svn/trunk/ googleurl
 cp SConstruct googleurl/src
 cd googleurl/base/
 patch ./basictypes.h < ../../basictypes.h.patch
