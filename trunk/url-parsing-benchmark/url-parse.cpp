1dab4630d227ad33b9811d9d1ee969fc7c3af590
whalebot.helmsman <whalebot.helmsman@bb6d7b18-8b01-a3bd-bbd2-4e41f94b8a27>
2010-05-12 23:47:00 +0000

    parse input file in separate function

--- trunk/url-parsing-benchmark/url-parse.cpp
+++ trunk/url-parsing-benchmark/url-parse.cpp
@@ -5,84 +5,82 @@
  * Created on May 12, 2010, 12:02 AM
  */
 
+#include <list>
 #include <fstream>
 #include <LinkFactory.h>
 #include <googleurl/src/gurl.h>
 
-/*
- * 
- */
+struct THtmlTask {
+    std::string m_sBaseUri;
+    typedef std::list<std::string>  TUriList;
+    TUriList    m_lUris;
+};
+
+typedef std::list<THtmlTask> THtmlTaskList;
+
+const char          kDelimiter          =   '\t';
+const std::string   kEmptyTabbedString  =   std::string(1, kDelimiter);
+
+void readTasksFromFile(std::ifstream& file, THtmlTaskList& tasks)
+{
+    
+    std::string     tmpStr("");
+    bool            continueRead(true);
+
+
+    //skip tabbed
+    while ((continueRead) and (std::getline(file, tmpStr))) {
+        if (not tmpStr.empty()) {
+            continueRead   =   (*tmpStr.begin() == kDelimiter);
+        }
+    }
+    
+    THtmlTask   task;
+    
+    task.m_sBaseUri =   tmpStr;
+    
+    
+    while (std::getline(file, tmpStr)) {
+
+        if ((tmpStr.empty()) or (tmpStr == kEmptyTabbedString)) {
+            continue;
+        }
+
+        if (*tmpStr.begin() != kDelimiter) {
+            tasks.push_back(task);
+
+            task.m_sBaseUri =   tmpStr;
+            task.m_lUris.clear();
+            continue;
+        }
+
+        task.m_lUris.push_back(tmpStr);
+    }
+}
+
+
+
 int main(int argc, char** argv) {
 
     if (argc < 2) {
         std::cout << argv[0] << " \"link file\"" << std::endl;
         return 1;
     }
+
     std::ifstream   file(argv[1]);
-    std::string     tmpStr("");
-    bool            _continue(true);
 
-    //skip tabbed
-    while ((_continue) and (std::getline(file, tmpStr))) {
-        if (not tmpStr.empty()) {
-            _continue   =   (*tmpStr.begin() == '\t');
+    THtmlTaskList   tasks;
+
+    readTasksFromFile(file, tasks);
+
+    for (THtmlTaskList::const_iterator task = tasks.begin(); task != tasks.end(); ++task) {
+        const THtmlTask::TUriList&  currentUris(task->m_lUris);
+        GURL    baseUrl(task->m_sBaseUri);
+
+        for (THtmlTask::TUriList::const_iterator uri = currentUris.begin(); uri != currentUris.end(); ++uri) {
+            GURL    relativeUrl(baseUrl.Resolve(*uri));
         }
-    }
-
-
-    std::string tmpStr3("http://");
-    tmpStr3 +=  tmpStr;
-    GURL    baseUrl(tmpStr3);
-
-    if (not baseUrl.is_valid()) {
-        std::cout << "'" << tmpStr3 << "' is invalid base url" << std::endl;
-    }
-
-    std::cout << "----------------------" <<std::endl;
-
-    while (std::getline(file, tmpStr)) {
-
-        if ((tmpStr.empty()) or (tmpStr == "\t")) {
-            continue;
-        }
-
-        if (*tmpStr.begin() != '\t') {
-
-
-            tmpStr3 =   "http://";
-            tmpStr3 +=  tmpStr;
-
-
-            GURL    anotherBaseUrl(tmpStr3);
-            baseUrl.Swap(&anotherBaseUrl);
-
-            if (not baseUrl.is_valid()) {
-                std::cout << "'" << tmpStr3 << "' is invalid base url" << std::endl;
-            }
-
-            std::cout << "----------------------" <<std::endl;
-
-            continue;
-        }
-
-        std::string tmpStr2(tmpStr.substr(1));
-        
-        GURL    relativeUrl(baseUrl.Resolve(tmpStr2));
-
-        if ((not relativeUrl.is_valid()) or (relativeUrl.is_empty())) {
-            std::cout << "'" << tmpStr2 << "' is invalid relative url" << std::endl;
-        }
-
-        std::cout << relativeUrl.host() << std::endl;
-        std::cout << relativeUrl.PathForRequest() << std::endl;
-
-        std::cout << "----------" <<std::endl;
-
-
-    }
-
-
-    
+    }    
 
     return 0;
 }
