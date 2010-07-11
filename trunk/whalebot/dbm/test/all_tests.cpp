d87c4c727170979e59afe81181227e8a7be73d4f
whalebot.helmsman <whalebot.helmsman@bb6d7b18-8b01-a3bd-bbd2-4e41f94b8a27>
2010-07-11 08:12:01 +0000

    start binary orm

---
+++ trunk/whalebot/dbm/test/all_tests.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "mapping_test.h"
+
+using namespace korm::test;
+
+int main ( int argc, char* argv[] )
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
